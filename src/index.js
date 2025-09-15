17afbce21be4e5ef70ac7cf8b21e3f316f6e8277
bahareh-bahrami <bahareh.bahramy@gmail.com>
2025-09-15 14:33:58 +0330

    add footer

--- src/index.js
+++ src/index.js
@@ -66,28 +66,20 @@ function Header() {
 }
 
 function Menu() {
+  const numPizza = pizzaData.length;
   return (
     <main className="menu">
       <h2>Our menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
-      {/* <Pizza
-        name="Pizza Focaccia"
-        ingredients="Bread with italian olive oil and rosemary"
-        photoSrc="pizzas/focaccia.jpg"
-        price={6}
-      />
-
-      <Pizza
-        name="Pizza Spinaci"
-        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-        photoSrc="pizzas/spinaci.jpg"
-        price={10}
-      /> */}
+      {numPizza > 0 ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. Come visit us later.</p>
+      )}
     </main>
   );
 }
@@ -108,13 +100,22 @@ function Pizza(props) {
 function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
-  const closeHour = 22;
+  const closeHour = 23;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open
+      {isOpen ? (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>
+          We're happy to meet you between {openHour}:00 and {closeHour}:00
+        </p>
+      )}
     </footer>
   );
 }
